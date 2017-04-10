7913acfa650b959e5958935c20d0f56dec2aafb7
Simon Lee <dev.simonlee@gmail.com>
2017-04-10 10:45:59 +0800

    Added within and all cafes endpoints for cafes and extracted query for different tables

--- server/routes/cafes.js
+++ server/routes/cafes.js
@@ -2,21 +2,45 @@ var express = require('express');
 var router = express.Router();
 var util = require('../util');
 var knex = require('../database');
-var bookshelf = require('bookshelf')(knex);
 var st = require('knex-postgis')(knex);
 
-var Cafes = bookshelf.Model.extend({
-  tableName: 'cafes'
-});
+function getAllPoints(tableName) {
+  return (req, res, next) => {
+    /*
+    Form query:
 
-/* GET cafes api */
-router.get('/:point/:k', function(req, res, next) {
-  let point = req.params['point'];
-  let k = req.params['k'];
+    select name, point, ST_Distance(
+      point,
+      'SRID=4326;Point(121.5618483 25.0376636)'
+    ) as distance
+    from cafes;
+    */
+    knex.select('*')
+    .from(tableName)
+    .then((data) => util.dbGeoParse(data))
+    .then((output) => {
+      console.log(output)
+      res.send(output)
+    })
+    .catch((err) => {
+      res.send(err);
+    })
+  }
+}
 
-  if (util.pointParamValid(point)) {
-    var [lat, lng] = util.pointFromParam(point);
-    console.log(`Requesting cafes around (${lat}, ${lng})`);
+function getKNearestPoints(tableName) {
+  return (req, res, next) => {
+    let point = req.params['point'];
+    let k = req.params['k'];
+    var lat, lng;
+    // Check point
+    if (util.pointParamValid(point)) {
+      [lat, lng] = util.pointFromParam(point);
+    } else {
+      res.send(`Invalid point parameter ${point}`);
+    }
+
+    console.log(`Requesting ${tableName} around (${lat}, ${lng})`);
 
     /*
     Form query:
@@ -33,7 +57,7 @@ router.get('/:point/:k', function(req, res, next) {
     */
     let wkt = `Point(${lng} ${lat})`;
     knex.select('name', 'point', st.distance(st.geography('point'), st.geography(st.geomFromText(wkt, 4326))).as('distance'))
-    .from('cafes')
+    .from(tableName)
     .orderByRaw(`point <-> 'SRID=4326;${wkt}'`)
     .limit(k)
     .then((data) => util.dbGeoParse(data))
@@ -44,9 +68,70 @@ router.get('/:point/:k', function(req, res, next) {
     .catch((err) => {
       res.send(err);
     })
-  } else {
-    res.send(`Invalid point parameter ${point}`);
   }
-});
+}
+
+function withinRadiusPoints(tableName) {
+  return (req, res, next) => {
+    let point = req.params['point'];
+    var radius = req.params['radius'];
+    var lat, lng;
+
+    // Check point
+    if (util.pointParamValid(point)) {
+      [lat, lng] = util.pointFromParam(point);
+    } else {
+      res.send(`Invalid point parameter ${point}`);
+    }
+
+    console.log(`Requesting ${tableName} within ${radius} meters from lat: ${lat}, lng: ${lng})`);
+
+    /*
+    Form query:
+
+    select name, point, ST_Distance(
+      point,
+      'SRID=4326;Point(121.5618483 25.0376636)'
+    ) as distance
+    from cafes
+    where ST_DWithin(
+      point::geography,
+      'SRID=4326;Point(121.5618483 25.0376636)'::geography,
+      600
+    );
+    */
+
+    let wkt = `Point(${lng} ${lat})`;
+    let geogPoint = st.geography('point');
+    let geogWkt = st.geography(st.geomFromText(wkt, 4326));
+    knex.select('name', 'point', st.distance(geogPoint, geogWkt).as('distance'))
+    .from(tableName)
+    .where(st.dwithin(geogPoint, geogWkt, radius))
+    .then((data) => util.dbGeoParse(data))
+    .then((output) => {
+      console.log(output)
+      res.send(output)
+    })
+    .catch((err) => {
+      res.send(err);
+    })
+  }
+}
+
+/*
+  All locations
+*/
+router.get('/', getAllPoints('cafes'));
+
+/*
+  Within distance
+*/
+router.get('/:point/within/:radius', withinRadiusPoints('cafes'));
+
+/*
+  GET cafes api
+  Returns info for the `k` closest cafes from coordinate `point`
+*/
+router.get('/:point/nearest/:k', getKNearestPoints('cafes'));
 
 module.exports = router;
